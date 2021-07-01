6355bf54a923de4ebecbd57ae5effd1f6a99fccb
zhouwenfeng <zhouwenfeng@agora.io>
2021-07-01 16:20:34 +0800

    update version

--- vm.go
+++ vm.go
@@ -222,5 +222,5 @@ func SnippetToAST(filename string, snippet string) (ast.Node, error) {
 
 // Version returns the Jsonnet version number.
 func Version() string {
-	return "v0.13.0"
+	return "v0.1.0-zsonnet"
 }
